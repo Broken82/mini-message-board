8e8e022bec69328c4e05b66651a003a8200f2018
Broken82 <zgrass11@gmail.com>
2025-08-20 20:33:44 +0200

    final commit

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -6,19 +6,33 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 
-
-
 indexRouter.get("/", (req, res) => {
-  res.send("Hello messageRouter!");
+  res.render("index", { messages: messages });
+});
+
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+
+indexRouter.post("/new", (req, res) => {
+  try {
+    const { text, user } = req.body;
+
+    messages.push({ text: text, user: user, added: new Date() });
+
+    res.redirect("/");
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 module.exports = indexRouter;
